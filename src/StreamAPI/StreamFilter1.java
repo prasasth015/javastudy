b2e8cbda68af63789a6a9270fe6a8b510f16b145
prasasth015 <prasathh017@gmail.com>
2024-03-23 20:50:23 +0530

    StreamAPI Examples

---
+++ src/StreamAPI/StreamFilter1.java
@@ -0,0 +1,19 @@
+package StreamAPI;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+//Fetch the names which have chars greater than 8 and less than 6
+public class StreamFilter1 {
+    public static void main(String[] args) {
+        List<String> name= Arrays.asList("Jonathan","Mayerss","Phillipe");
+        List<String> result=new ArrayList<>();
+        result=name.stream().filter(str->str.length()>6 && str.length()<8).collect(Collectors.toList());
+        System.out.println(result);
+
+        //without using result variable
+        name.stream().filter(str->str.length()>6 && str.length()<8).forEach(System.out::println);
+    }
+}
