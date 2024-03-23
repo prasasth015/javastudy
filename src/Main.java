7bc303630df842053c85e09ce64981fe154a1ef5
prasasth015 <prasathh017@gmail.com>
2024-03-23 20:28:56 +0530

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<String> s=new ArrayList<>();
+        s.add("Hari");
+
     }
 }
