db7bc793127745fb3c25fb97add948dc0ee9c7f7
prasasth015 <prasathh017@gmail.com>
2025-11-12 14:01:42 +0530

    second commit

--- src/StreamAPI/StreamFilter1.java
+++ src/StreamAPI/StreamFilter1.java
@@ -10,10 +10,10 @@ public class StreamFilter1 {
     public static void main(String[] args) {
         List<String> name= Arrays.asList("Jonathan","Mayerss","Phillipe");
         List<String> result=new ArrayList<>();
-        result=name.stream().filter(str->str.length()>6 && str.length()<8).collect(Collectors.toList());
+        result=name.stream().filter(str-> str.length() == 7).collect(Collectors.toList());
         System.out.println(result);
 
         //without using result variable
-        name.stream().filter(str->str.length()>6 && str.length()<8).forEach(System.out::println);
+        name.stream().filter(str-> str.length() == 7).forEach(System.out::println);
     }
 }
